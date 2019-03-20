9863ca9a76ab0ec6886dfbc807a9bbd2325781bf
hmm <hmmChase@users.noreply.github.com>
2019-03-20 13:59:05 -0700

    Update

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,25 +1,24 @@
-// import { Query } from 'react-apollo';
-// import * as booksQL from '../graphql/Queries/books';
+import { Query } from 'react-apollo';
+import * as booksQL from '../graphql/Queries/books';
 
 class IndexPage extends React.Component {
   render() {
     return (
-      <h1>Hello</h1>
-      // <Query query={booksQL.BOOKS_QUERY}>
-      //   {res => {
-      // console.log(' : ----------------------------------');
-      // console.log(' : IndexPage -> render -> res', res);
-      // console.log(' : ----------------------------------');
+      <Query query={booksQL.BOOKS_QUERY}>
+        {res => {
+          console.log(' : ----------------------------------');
+          console.log(' : IndexPage -> render -> res', res);
+          console.log(' : ----------------------------------');
 
-      //       return (
-      //         <ul>
-      //           {res.data.books.map(book => (
-      //             <li key={book.id}>{book.title}</li>
-      //           ))}
-      //         </ul>
-      //       );
-      //     }}
-      //   </Query>
+          return (
+            <ul>
+              {res.data.books.map(book => (
+                <li key={book.id}>{book.title}</li>
+              ))}
+            </ul>
+          );
+        }}
+      </Query>
     );
   }
 }
