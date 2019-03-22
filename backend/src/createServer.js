90473a3c0c7e4b234d1d4c646a801f1bc528ebed
hmm <hmmChase@users.noreply.github.com>
2019-03-22 10:25:07 -0700

    Update

--- backend/src/createServer.js
+++ backend/src/createServer.js
@@ -1,20 +1,41 @@
 import { ApolloServer, gql, makeExecutableSchema } from 'apollo-server-express';
-import { importSchema } from 'graphql-import';
-const path = require('path');
+// import { importSchema } from 'graphql-import';
+// const path = require('path');
 import { prisma } from '../prisma/generated/prisma-client';
-import resolvers from './resolvers';
-const typeDefs = importSchema(path.resolve('./src/schema/schema.graphql'));
+// import resolvers from './resolvers';
+// const typeDefs = importSchema(path.resolve('./src/schema/schema.graphql'));
 // console.log('TCL: typeDefs', typeDefs);
 
-// const gqlTypeDefs = gql`
-//   ${typeDefs}
-// `;
+// const schema = makeExecutableSchema({ typeDefs, resolvers });
 
-const schema = makeExecutableSchema({ typeDefs, resolvers });
+const typeDefs = gql`
+  type User {
+    id: ID!
+    email: String!
+    password: String!
+  }
+
+  type Query {
+    user(id: ID!): User
+    users: [User]!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    user: (parent, args, ctx, info) => {
+      return ctx.prisma.user({ id: args.id });
+    },
+
+    users: (parent, args, ctx, info) => {
+      return ctx.prisma.users();
+    }
+  }
+};
 
 export default () =>
   new ApolloServer({
-    schema,
+    typeDefs,
     resolvers,
     context: ({ req, res }) => {
       return { req, res, prisma };
