abf5e2fcfe7d35c42506aefa99b74a359dc0cb7e
hmm <hmmChase@users.noreply.github.com>
2019-03-22 09:47:24 -0700

    Update

--- backend/src/createServer.js
+++ backend/src/createServer.js
@@ -1,14 +1,24 @@
-import { ApolloServer } from 'apollo-server-express';
+import { ApolloServer, gql } from 'apollo-server-express';
 import { importSchema } from 'graphql-import';
+const path = require('path');
 import { prisma } from '../prisma/generated/prisma-client';
 import resolvers from './resolvers';
-const path = require('path');
-const typeDefs = importSchema(path.join(__dirname, './schema/schema.graphql'));
-console.log('TCL: typeDefs', typeDefs);
+const typeDefs = importSchema(path.resolve('./src/schema/schema.graphql'));
+// console.log('TCL: typeDefs', typeDefs);
+
+const gqlTypeDefs = gql`
+  ${typeDefs}
+`;
+
+// import { makeExecutableSchema } from 'graphql-tools';
+
+// const typeDefs = importSchema('./src/schema/schema.graphql');
+
+// const schema = makeExecutableSchema({ typeDefs, resolvers });
 
 export default () =>
   new ApolloServer({
-    typeDefs,
+    typeDefs: gqlTypeDefs,
     resolvers,
     context: ({ req, res }) => {
       return { req, res, prisma };
